7189d05998bbdb702bbf8ee092a42bb614421cd1
Lindsay Gaff <lindsaygaff@gmail.com>
2013-05-29 22:48:56 +1000

    Using real mode registers for magic break trigger

--- boot3/source/stub.adb
+++ boot3/source/stub.adb
@@ -2,7 +2,11 @@ with System.Machine_Code; use System.Machine_Code;
 
 procedure Stub is
 begin
-   Asm ("xchg %%ebx, %%ebx",
-       Volatile => True);
+   Asm ("xchg %%bx, %%bx",
+        Volatile => True);
+   loop
+      Asm ("nop",
+           Volatile => True);
+   end loop;
 end Stub;
 pragma No_Return (Stub);
