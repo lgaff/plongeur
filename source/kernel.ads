f54983a2810ffafff97e33f71268f2ac290f8ad6
Lindsay Gaff <lindsaygaff@gmail.com>
2013-06-04 17:21:25 +1000

    Shuffled entry point to plongeur.adb

--- source/kernel.ads
+++ source/kernel.ads
@@ -1,5 +1,11 @@
 with Interfaces; use Interfaces;
+with System;
+with Ada.Unchecked_Conversion;
 
 package Kernel is
+      function To_Unsigned_32 is new Ada.Unchecked_Conversion
+     (Source => System.Address,
+      Target => Unsigned_32);
+
    Cksum : Unsigned_32 := 16#DEAD#;
 end Kernel;
--- source/plongeur.adb
+++ source/plongeur.adb
@@ -1,15 +1,16 @@
 with System.Machine_Code; use System.Machine_Code;
 with Interfaces; use Interfaces;
+with Kernel; use Kernel;
 
 procedure Plongeur is
 begin
    Asm ("xchg %%bx, %%bx",
         Volatile => True);
-   Asm ("mov %%eax, %0",
+   Asm ("mov %%ebx, %0",
         Inputs => Unsigned_32'Asm_Input ("a", Cksum),
         Volatile => True);
-   Asm ("mov %%ebx, %0",
-        Inputs => Unsigned_32'Asm_Input ("a",
+   Asm ("mov %%ecx, %0",
+        Inputs => Unsigned_32'Asm_Input ("b",
                                          To_Unsigned_32 (Cksum'Address)),
         Volatile => True);
    Asm ("xchg %%bx, %%bx",
