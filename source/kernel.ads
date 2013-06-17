172e32afb1f0d35b38698c8eb63f490dcbbfb93a
Lindsay Gaff <lindsaygaff@gmail.com>
2013-06-17 15:15:31 +1000

    Kernel_Fake_Base formatting incorrect

--- source/kernel.ads
+++ source/kernel.ads
@@ -1,9 +1,6 @@
 with Interfaces; use Interfaces;
-with System;
-with Ada.Unchecked_Conversion;
 
 package Kernel is
-
       Cksum : Unsigned_32 := 16#DEAD#;
-      Kernel_Fake_Base : constant Natural := 0x40000000;
+      Kernel_Fake_Base : constant Unsigned_32 := 16#40000000#;
 end Kernel;
