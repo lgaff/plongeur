c3c6798e1995e8845130e5a96eb6d8e94f9e275d
Lindsay Gaff <lindsaygaff@gmail.com>
2014-05-03 02:59:12 +1000

    New setup/run scripts, changes to build pipe for grub 2

--- buildimage.sh
+++ buildimage.sh
@@ -3,4 +3,6 @@ gnatmake -Pgnat.gpr
 nasm -felf -o objects/entry.o source/entry.s
 gnatmake --RTS=`pwd`/ada_runtime -Pplongeur.gpr
 cp plongeur isodir/boot
-grub-mkrescue -o plongeur.iso isodir
+sudo mount disk.img /mnt
+sudo cp -rv isodir/boot /mnt
+sudo umount /mnt
---
+++ run-plongeur.sh
@@ -0,0 +1,1 @@
+qemu-system-i386 -hda disk.img
