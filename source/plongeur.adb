c06df2d3db2a731bd70d1d535be2989f0f6f8a50
Lindsay Gaff <lindsaygaff@gmail.com>
2014-05-04 23:27:32 +1000

    Cleaning up old debug code

--- source/plongeur.adb
+++ source/plongeur.adb
@@ -1,5 +1,4 @@
 with System.Machine_Code; use System.Machine_Code;
-with Interfaces; use Interfaces;
 with Kernel; use Kernel;
 with Kernel.Descriptor_Tables; use Kernel.Descriptor_Tables;
 with Kernel.Initialisation; use Kernel.Initialisation;
@@ -8,23 +7,11 @@ with Console; use Console;
 
 procedure Plongeur is
 begin
-   Asm ("xchg %%bx, %%bx",
-        Volatile => True);
    Go_To_Higher_Half (Kernel_Page_Directory, Identity_Mapped_Table);
-   Asm ("xchg %%bx, %%bx",
-        Volatile => True);
    Install_GDT (Gp, Global_Descriptor_Table);
-   Asm ("mov %%ebx, %0",
-        Inputs => Unsigned_32'Asm_Input ("a", Cksum),
-        Volatile => True);
-   Asm ("mov %%ecx, %0",
-        Inputs => Unsigned_32'Asm_Input ("b",
-                                         To_Unsigned_32 (Cksum'Address)),
-        Volatile => True);
-   Asm ("xchg %%bx, %%bx",
-        Volatile => True);
    Blank;
-   Write ("Hello, World!", Screen_Width'First, Screen_Height'First);
+   Magic_Break;
+   Write ("Hello, World!");
    loop
       Asm ("nop",
            Volatile => True);
