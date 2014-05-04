45a4ec062602743a9dd6c6655c47ccfbeac1892e
Lindsay Gaff <lindsaygaff@gmail.com>
2014-05-04 23:28:08 +1000

    Port IO Routines

--- source/kernel-utilities.adb
+++ source/kernel-utilities.adb
@@ -1,4 +1,5 @@
 with Interfaces;
+with System.Machine_Code; use System.Machine_Code;
 
 package body Kernel.Utilities is
    function Align_Address (Physical_Address : Unsigned_32)
@@ -10,4 +11,49 @@ package body Kernel.Utilities is
          Amount => 12);
       return Page_Address (Temp mod 2 ** 20);
    end Align_Address;
+
+   procedure Write_Port (Port : Unsigned_16;
+                  Value : in Unsigned_8) is
+   begin
+      Asm ("outb %0, %1",
+         Inputs => (Unsigned_8'Asm_Input ("a", Value),
+                    Unsigned_16'Asm_Input ("dN", Port)),
+         Volatile => True);
+   end Write_Port;
+   pragma Inline (Write_Port);
+   procedure Write_Port (Port : in Unsigned_16;
+                  Value : in Unsigned_16) is
+   begin
+      Asm ("outw %0, %1",
+         Inputs => (Unsigned_16'Asm_Input ("a", Value),
+                    Unsigned_16'Asm_Input ("dN", Port)),
+         Volatile => True);
+   end Write_Port;
+   pragma Inline (Write_Port);
+   function Read_Port (Port : in Unsigned_16) return Unsigned_8 is
+      Return_Value : Unsigned_8;
+   begin
+      Asm ("inb %1, %0",
+         Inputs => Unsigned_16'Asm_Input ("dN", Port),
+         Outputs => Unsigned_8'Asm_Output ("=a", Return_Value),
+         Volatile => True);
+      return Return_Value;
+   end Read_Port;
+   pragma Inline (Read_Port);
+   function Read_Port (Port : in Unsigned_16) return Unsigned_16 is
+      Return_Value : Unsigned_16;
+   begin
+      Asm ("inw %1, %0",
+         Inputs => Unsigned_16'Asm_Input ("dN", Port),
+         Outputs => Unsigned_16'Asm_Output ("=a", Return_Value),
+         Volatile => True);
+      return Return_Value;
+   end Read_Port;
+   pragma Inline (Read_Port);
+   procedure Magic_Break is
+   begin
+      Asm ("xchg %%bx, %%bx",
+         Volatile => True);
+   end Magic_Break;
+   pragma Inline (Magic_Break);
 end Kernel.Utilities;
--- source/kernel-utilities.ads
+++ source/kernel-utilities.ads
@@ -9,6 +9,30 @@ with Ada.Unchecked_Conversion;
 with System;
 
 package Kernel.Utilities is
+   type Double_Word is record
+      High_Word : Unsigned_16;
+      Low_Word : Unsigned_16;
+   end record;
+   for Double_Word'Size use 32;
+
+   type Double_Byte is record
+      High_Byte : Unsigned_8;
+      Low_Byte : Unsigned_8;
+   end record;
+   for Double_Byte'Size use 16;
+
+   function To_Double_Word is new Ada.Unchecked_Conversion
+      (Source => Unsigned_32,
+       Target => Double_Word);
+
+   function To_Double_Word is new Ada.Unchecked_Conversion
+      (Source => Standard.Integer,
+       Target => Double_Word);
+
+   function To_Double_Byte is new Ada.Unchecked_Conversion
+      (Source => Unsigned_16,
+       Target => Double_Byte);
+
    function To_Global_Descriptor is new Ada.Unchecked_Conversion
      (Source => Unsigned_64,
       Target => Global_Descriptor);
@@ -29,4 +53,11 @@ package Kernel.Utilities is
 --        Virtual_Address : Page_Address;
 --        Padding : Unsigned_12;
 --     end record;
+   procedure Write_Port (Port : Unsigned_16;
+                  Value : Unsigned_8);
+   procedure Write_Port (Port : in Unsigned_16;
+                  Value : in Unsigned_16);
+   function Read_Port (Port : in Unsigned_16) return Unsigned_16;
+   function Read_Port (Port : in Unsigned_16) return Unsigned_8;
+   procedure Magic_Break;
 end Kernel.Utilities;
